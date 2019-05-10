7a7be8df65e561726102644b3cec06d01e1b5697
Draxter555 <global.elite.no@gmail.com>
2019-05-10 18:46:34 +0300

    new1

--- shop.py
+++ shop.py
@@ -19,9 +19,22 @@ class Coca_Cola(Goods):
             print('Вы купили этот товар(', coca_cola.name, ')!')
 
 
+class Choco_Pie(Goods):
+    def __init__(self):
+        super().__init__(name='Чоко Пай', value=49)
+
+    def get_good(self, basket):
+        choco_pie = Choco_Pie()
+        basket.items.append(choco_pie)
+        if choco_pie in basket.items:
+            print('Вы купили этот товар(', choco_pie.name, ')!')
+
+
 basket = Basket()
 print('Добро пожаловать в магазин!')
-print('Список имеющихся товаров:\n1 - Кока Кола\n2 - ...\n3 - ...')
+print('Список имеющихся товаров:\n1 - ', Coca_Cola().name, '\n2 - ', Choco_Pie().name, '\n3 - ...')
 choice = int(input('Чего желаете?\n'))
 if choice == 1:
     Coca_Cola().get_good(basket)
+if choice == 2:
+    Choco_Pie().get_good(basket)
