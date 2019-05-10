fafd8d4f8a89b205cf90d24f88201947e1bd2493
Draxter555 <global.elite.no@gmail.com>
2019-05-10 18:33:37 +0300

    new1

--- game.py
+++ game.py
@@ -1,2 +1,27 @@
-print('Hello')
-print('Good bye')
+class Basket():
+    def init(self):
+        self.items = []
+
+class Goods():
+    def init(self, name, value):
+        self.name = name
+        self.value = value
+
+
+class Coca_Cola(Goods):
+    def init(self):
+        super().init(name='Кока Кола', value=49)
+
+    def get_good(self, basket):
+        coca_cola = Coca_Cola()
+        basket.items.append(coca_cola)
+        if coca_cola in basket.items:
+            print('Вы купили этот товар(', coca_cola.name, ')!')
+
+
+basket = Basket()
+print('Добро пожаловать в магазин!')
+print('Список имеющихся товаров:\n1 - Кока Кола\n2 - ...\n3 - ...')
+choice = int(input('Чего желаете?\n'))
+if choice == 1:
+    Coca_Cola().get_good(basket)
